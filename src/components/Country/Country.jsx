d237692ca875b99cb20936c4d79e115c24be6490
abutalha08 <taufique752@gmail.com>
2026-03-25 01:41:57 +0600

    Use style in react and 3 columns layout and conditional text

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country }) => {
     console.log(country.flags.flags.png)
+
+    const handleVisited = ()=>{
+        console.log('btn clicked')
+    }
     return (
-        <div>
-            <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+        <div className='country'>
+            <img className='img' src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h2>Name: {country.name.common}</h2>
             <p>Population: {country.population.population} </p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"} </p>
+            <button className='btn' onClick={handleVisited}>Not Visited</button>
+
         </div>
     );
 };
